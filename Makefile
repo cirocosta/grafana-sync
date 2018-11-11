ebba5e18124f5311f31fa37e8238d155b1965750
Ciro S. Costa <cscosta@pivotal.io>
2018-11-10 20:05:55 -0500

    Adds automatic directory creation

--- Makefile
+++ Makefile
@@ -2,4 +2,7 @@ build:
 	go build -v -i
 
 run:
-	./grafana-sync --verbose --access-token=eyJrIjoiMHJZYlEyd1M2dXZON01hTmdQTG5kR29wVjJZUDhhVjciLCJuIjoidmlld2VyIiwiaWQiOjF9
+	./grafana-sync \
+		--verbose \
+		--access-token=eyJrIjoiMHJZYlEyd1M2dXZON01hTmdQTG5kR29wVjJZUDhhVjciLCJuIjoidmlld2VyIiwiaWQiOjF9 \
+		--directory=/tmp
--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"github.com/pkg/errors"
+	"os"
+	"path"
 
 	"github.com/cirocosta/grafana-sync/grafana"
 	"github.com/jessevdk/go-flags"
@@ -13,6 +15,33 @@ var config struct {
 	Address     string        `long:"address" default:"http://localhost:3000" description:"grafana address"`
 	Directory   DirectoryFlag `long:"directory" default:"./" description:"directory where dashboards live"`
 	Verbose     bool          `short:"v" long:"verbose" description:"displays requests on stderr"`
+}
+
+func eventuallyCreateDirectory(dir string) (err error) {
+	finfo, err := os.Stat(dir)
+	if err != nil {
+		if os.IsNotExist(err) {
+			err = os.MkdirAll(dir, 0755)
+			if err != nil {
+				err = errors.Wrapf(err,
+					"failed to create directory %s", dir)
+				return
+			}
+
+			return
+		}
+
+		err = errors.Wrapf(err,
+			"unexpected failure checking directory %s", dir)
+		return
+	}
+
+	if !finfo.IsDir() {
+		err = errors.Errorf("location %s already exists and is not a directory", dir)
+		return
+	}
+
+	return
 }
 
 func main() {
@@ -27,10 +56,14 @@ func main() {
 		panic(err)
 	}
 
-	// perform some validations:
-	// 1. does the directory exist?
-
 	for _, dashboard := range dashboards {
-		fmt.Printf("%+v\n", dashboard)
+		dashboardFolderInFs := path.Join(string(config.Directory), dashboard.Folder)
+
+		err = eventuallyCreateDirectory(dashboardFolderInFs)
+		if err != nil {
+			panic(err)
+		}
+
+		// update the file with the new dashboard.
 	}
 }
